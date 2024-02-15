e77ff9fbd043f25f1c90d58bd3c5a748ab438154
Arsentiy Karevskiy <arsentiy.karevskiy@corp.itibo.com>
2024-02-15 11:44:24 +0300

    update sql request for weather table

--- task2.1.sql
+++ task2.1.sql
@@ -1,25 +1,20 @@
-with distinct_data as (
-	select distinct  city, dt, "hour", temperature_c, pressure, is_rainy
-	from weather_tab
-	), data_with_rain_flag as 
-	(
-select distinct city, is_rainy, dt, "hour", 	
-	case 
-		when (is_rainy = 1 and lead_rainy = 0) then 1
-		else 0
-		end as flag
-from (
-	select city, dt, "hour", temperature_c, pressure, is_rainy,
-	lead(is_rainy) over (partition by city order by city, dt, "hour") as lead_rainy from distinct_data
-) as data_with_rain_flag
-order by city, dt, "hour"
-)
-select city, dt, start_hour, end_hour 
-from (
-	select distinct city, dt, 
-	min(hour)over(partition by city,dt) as start_hour,
-	min(case 
-		when flag = 1 then hour
-	end) over(partition by city,dt) as end_hour
-	from data_with_rain_flag where is_rainy = 1) as rain_data
-
+with start_data as(
+	select city, dt, is_rainy,
+	min(hour) as start_hour
+	from weather_tab 
+	group by city,dt, is_rainy
+	having is_rainy = 1), 
+	end_data as(
+	select city,dt,min(hour) as end_hour from weather_tab
+	where hour > (
+		select start_hour 
+		from start_data
+		where (weather_tab.city = start_data.city and weather_tab.dt = start_data.dt)
+	)
+	group by city, dt, is_rainy
+	having is_rainy = 0)
+select start_data.city, start_data.dt, start_hour, end_hour
+from start_data
+left join end_data 
+on (start_data.city = end_data.city and start_data.dt = end_data.dt)
+order by city,dt
